f57fe82a97455923523a21662929f648e6fbec84
AaryaTalgaonkar <125786542+AaryaTalgaonkar@users.noreply.github.com>
2024-08-28 17:31:23 +0530

    sound_similar checks repeated phenomes

--- solution.py
+++ solution.py
@@ -2,61 +2,51 @@
 import random
 import math
 import json
-def load_dictionary(file_path):
-    with open(file_path, 'r') as file:
-        valid_words = set(word.strip().upper() for word in file)
-    return valid_words
-
-def is_spelled_correctly(word, valid_words):
-    return word.upper() in valid_words
-
-# Load dictionary once
-
 class Agent(object):
     def __init__(self, phoneme_table, vocabulary) -> None:
         """
         Your agent initialization goes here. You can also add code but don't remove the existing code.
         """
-        self.phoneme_table = phoneme_table
+        reversed_table={}
+        for key, value in phoneme_table.items():
+            for v in value:
+                if(v in reversed_table):
+                    reversed_table[v].append(key)
+                else:
+                    reversed_table[v]=[key]
+        self.phoneme_table = reversed_table
         self.vocabulary = vocabulary
         self.best_state = None
-        self.valid_words = load_dictionary('words.txt')
-   
-
-    def sound_similar(self,init_sentence,environment):
-        
+    def sound_similar(self,init_sentence,environment):       
         init_sentence=init_sentence.upper()
-        words_list=init_sentence.split()
-        init_cost=1e9
-        
-        for i in range(0,len(words_list)):
-            w=words_list[i]
-            print(w)
-            for key,value in self.phoneme_table.items():
-                
-
-                for v in value:
-                    orig=w
-                    lower_v=v.upper()
-                    lower_key=key.upper()
-                    
-                    new_string = orig.replace(lower_v,lower_key )
-                 
-                    if(new_string!=orig ):
-                        new_list=words_list
-                        new_list[i]=new_string
-                       
-                        new_sentence=' '.join(new_list)
-                        cost=environment.compute_cost(new_sentence)
-                        
-                        if(cost<init_cost):
-                            init_sentence=new_sentence
-                            init_cost=cost
-                        new_list[i]=orig
-
+        orig_sentence=init_sentence
+        init_cost=1e9  
+        length=len(orig_sentence)
+        substr_len=1
+        for i in range (0,length):
+            phoneme=orig_sentence[i:i+substr_len]
+            key=phoneme
+            if(key in self.phoneme_table):
+                values=self.phoneme_table[key]
+                for v in values:
+                    new_sentence=orig_sentence[:i]+v+orig_sentence[(i+substr_len):]
+                    cost=environment.compute_cost(new_sentence)
+                    if(cost<init_cost):
+                        init_cost=cost
+                        init_sentence=new_sentence
+        substr_len=2  
+        for i in range (0,length-1):
+            phoneme=orig_sentence[i:i+substr_len]
+            key=phoneme
+            if(key in self.phoneme_table):
+                values=self.phoneme_table[key]
+                for v in values:
+                    new_sentence=orig_sentence[:i]+v+orig_sentence[(i+substr_len):]
+                    cost=environment.compute_cost(new_sentence)
+                    if(cost<init_cost):
+                        init_cost=cost
+                        init_sentence=new_sentence                          
         return init_cost,init_sentence
-                            
-
     def vocab(self,init_sentence,environment):
         init_sentence=init_sentence.upper()
         orig_sentence=init_sentence
@@ -125,48 +115,28 @@ class Agent(object):
         Your ASR corrector agent goes here. Environment object has following important members.
         - environment.init_state: Initial state of the environment. This is the text that needs to be corrected.
         - environment.compute_cost: A cost function that takes a text and returns a cost. E.g., environment.compute_cost("hello") -> 0.5
-
         Your agent must update environment.best_state with the corrected text discovered so far.
         """
- 
-
         self.best_state = environment.init_state
-        prev_sentence=" "
         cost = 1e9
-        i=10
         changed_sentence=self.best_state
-        print(self.best_state)
-        
-        print(cost)
         new_cost=0
-        temperature=1000
-        cooling_rate=0.8
-        max_iterations=10
+        max_iterations=9
         while(max_iterations>0):
-            # prev_sentence=self.best_state
-
             new_cost,changed_sentence=self.sound_similar(changed_sentence,environment)
-            print(changed_sentence,new_cost)
-            delta_value=new_cost-cost
-            if new_cost<cost or random.uniform(0, 1) < math.exp(-delta_value / temperature):
+            if new_cost<cost :
+                print(changed_sentence)
+                print(new_cost)
+                print(10-max_iterations)
                 cost=new_cost
                 self.best_state=changed_sentence
                 environment.best_state=changed_sentence
-            temperature*=cooling_rate
             max_iterations-=1
-        new_cost=0
-       
+        new_cost=0 
         changed_sentence=self.best_state
-       
-        
         new_cost,changed_sentence=self.vocab(changed_sentence,environment)
-        # print(new_cost," ",changed_sentence)
         if(new_cost<cost):
             self.best_state=changed_sentence
             environment.best_state=changed_sentence
-
-        print(self.best_state)
-
-        print(cost)
 
     
